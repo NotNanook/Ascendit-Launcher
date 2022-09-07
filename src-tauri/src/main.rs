6311ba08b8e08e6c41386ead2aed901f99562559
NotNanook <gogabrys@gmail.com>
2022-09-07 11:34:38 +0200

    added config file support

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,11 +4,29 @@
   windows_subsystem = "windows"
 )]
 
-use std::{io::{Cursor, Read}, fs::File, path::Path};
+use std::{io::{Cursor}, path::Path};
 use sha256::{digest_file};
+use serde_derive::{Deserialize, Serialize};
+use std::process::Command;
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 extern crate directories;
 use directories::{ProjectDirs};
+
+#[derive(Deserialize, Serialize, Debug)]
+struct RaotConfig {
+  path: String,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+struct MinecraftConfig {
+  path: String,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+struct ConfigFile {
+  raot: RaotConfig,
+  minecraft: MinecraftConfig,
+}
 
 fn main() 
 {
@@ -22,8 +40,7 @@ async fn check_update(url: String, game: &str) -> Result<()> {
   if let Some(proj_dirs) = ProjectDirs::from("", "",  "Ascendit Launcher") {
     // get path to folder of gamedll
     let proj_path= proj_dirs.data_dir();
-    let prefix = proj_path.parent().unwrap();
-    std::fs::create_dir_all(prefix).unwrap();
+    std::fs::create_dir_all(proj_path).unwrap();
 
     // download most recent hash from server
     let game_hash_str: &str = &("hash".to_owned() + game + &(".txt".to_owned()));
@@ -41,6 +58,11 @@ async fn check_update(url: String, game: &str) -> Result<()> {
         let mut content =  Cursor::new(response.bytes().await?);
         std::io::copy(&mut content, &mut file)?;
       }
+    } else {
+      let response = reqwest::get(url + "Ascendit-" + game + ".dll").await?;
+      let mut file = std::fs::File::create(dll_path)?;
+      let mut content =  Cursor::new(response.bytes().await?);
+      std::io::copy(&mut content, &mut file)?;
     }
 
     Ok(())
@@ -49,13 +71,61 @@ async fn check_update(url: String, game: &str) -> Result<()> {
   }
 }
 
+fn create_config(path: &Path) {
+  let raot_path: String = "C:/Users/".to_owned() + whoami::username().as_str() + "/AppData/Local/RAoT/raot.exe";
+  let minecraft_path: String = String::from("");
+
+  let mut config_json: ConfigFile = serde_json::from_str(r#"{"raot": {"path": ""},"minecraft" : {"path": ""}}"#).unwrap();
+  config_json.raot.path = raot_path;
+  config_json.minecraft.path = minecraft_path;
+
+  let _ = std::fs::write(path, serde_json::to_string_pretty(&config_json).unwrap());
+}
+
+fn find_path(game: String) -> String {
+  if let Some(proj_dirs) = ProjectDirs::from("", "",  "Ascendit Launcher") {
+    let proj_path= proj_dirs.data_dir();
+    let config_file: &str = "ascendit-config.json";
+    let config_path_buf = proj_path.join(config_file);
+    let config_path = config_path_buf.as_path();
+
+    match config_path.exists() {
+      true => (),
+      false => create_config(config_path),
+    }
+
+    let config_json = {
+      let config_json = std::fs::read_to_string(&config_path).unwrap();
+      serde_json::from_str::<ConfigFile>(&config_json).unwrap()
+    };
+    
+    if game == "raot".to_string() {
+      return config_json.raot.path;
+    } else if game == "minecraft".to_string() {
+      return config_json.minecraft.path;
+    } else {
+      return "".to_owned();
+    }
+
+  } else {
+    return "".to_owned();
+  }
+}
+
+fn execute(path: String) {
+  Command::new(path).output().expect("Failed to execute process");
+}
+
 #[tauri::command]
 async fn inject_raot()
 {
   // check for updates
-  let _check_result = check_update("https://v45600.1blu.de/launcherFiles/".to_string(), "Raot").await.unwrap();
+  check_update("https://v45600.1blu.de/launcherFiles/".to_string(), "Raot").await.unwrap();
   
-  // execute
+  // find path and execute
+  let path: String = find_path("raot".to_owned());
+  execute(path);
+  
   // inject
 }
 
